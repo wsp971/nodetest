77fad5c1aa1e054d32603e84f4bd768c93a6502a
wangshiping <wsp971@qq.com>
2016-03-23 02:14:17 +0800

    a node test

---
+++ nodetest1/routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});
+
+router.get('/helloworld', function(req, res, next) {
+  res.render('helloworld', { title: 'helow world!' });
+});
+module.exports = router;
