520e1176c88cd418ab2604d7b23dd5edd35a83a8
wangshiping <wsp971@qq.com>
2016-03-30 23:31:25 +0800

    use mongodb to insert an query data

--- nodetest1/routes/index.js
+++ nodetest1/routes/index.js
@@ -6,6 +6,17 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/*router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    console.log(collection);
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
+});*/
+
 router.get('/userlist', function(req, res) {
     var db = req.db;
     var collection = db.get('usercollection');
@@ -18,5 +29,24 @@ router.get('/userlist', function(req, res) {
 
 router.get('/helloworld', function(req, res, next) {
   res.render('helloworld', { title: 'helow world!' });
+});
+router.get('/newuser',function(req,res){
+	res.render('newuser',{'title':'add new user'});
+});
+router.post('/adduser',function(req,res){
+	var db=req.db;
+	var userName=req.body.username;
+	var userEmail=req.body.useremail;
+	var collection=db.get('usercollection');
+	collection.insert({
+		'username':userName,
+		'email':userEmail
+	},function(err,doc){
+		if(err){
+			res.send("There wos a problem adding the information to the database");
+		}else{
+			res.redirect('userlist');
+		}
+	});
 });
 module.exports = router;
